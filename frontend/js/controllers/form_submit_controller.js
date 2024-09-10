6bc41448914d573a66cef72f45a0da15688c264d
Moski Doski <abushaikh@gmail.com>
2024-09-10 16:20:39 +0400

    Testing a new cntroller

---
+++ frontend/js/controllers/form_submit_controller.js
@@ -0,0 +1,30 @@
+import ApplicationController from "./application_controller";
+
+export default class extends ApplicationController {
+  connect() {
+    this.element.addEventListener(
+      "turbo:before-fetch-response",
+      this.handleFormResponse,
+    );
+  }
+
+  disconnect() {
+    this.element.removeEventListener(
+      "turbo:before-fetch-response",
+      this.handleFormResponse,
+    );
+  }
+
+  handleFormResponse(e) {
+    const response = e.detail.fetchResponse.response;
+
+    console.log(response.status);
+
+    // if (response.status >= 500) {
+    //   e.preventDefault();
+
+    //   const title = `${response.status} (${response.statusText})`;
+    //   response.text().then((content) => ErrorModal.show({ title, content }));
+    // }
+  }
+}
